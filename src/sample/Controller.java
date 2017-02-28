085c6fc3c14e7a3cd81b1574338ca0e7fff1d9c1
Parker <parker@cowlicked.com>
2017-02-28 11:40:20 -0500

    Adding Scenes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class Controller {
+    @FXML Button button1;
+    @FXML Button button2;
+
+    public void handleButtonClick1(){
+
+    }
+
+    public void handleButtonClick2(){
+
+    }
+
 }
---
+++ src/sample/SceneController2.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by parker on 2/28/17.
+ */
+public class SceneController2 {
+    @FXML
+    Button button;
+
+    public void handleButtonClick(){
+
+    }
+}
