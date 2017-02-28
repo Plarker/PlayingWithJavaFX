e43cf5120fd63d7682f6bbcd8992322889301dc0
Parker <parker@cowlicked.com>
2017-02-28 17:49:52 -0500

    Added ability to change scenes on a stage

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,37 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Controller {
-    @FXML Button button1;
-    @FXML Button button2;
+    @FXML private Button button1;
+    @FXML private Button button2;
+    private Stage stage;
+    private Parent root;
 
-    public void handleButtonClick1(){
+    public void handleButtonClick1() throws IOException{
+        changeScene("scene1.fxml","Scene 1");
 
     }
 
-    public void handleButtonClick2(){
+    public void handleButtonClick2() throws IOException{
+        changeScene("scene2.fxml","Scene 2");
+    }
 
+    private void changeScene(String link, String title) throws IOException{
+        stage=(Stage) button1.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource(link));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
     }
 
 }
--- src/sample/SceneController2.java
+++ src/sample/SceneController2.java
@@ -1,16 +1,34 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by parker on 2/28/17.
  */
 public class SceneController2 {
     @FXML
-    Button button;
+    private Button button;
+    private Stage stage;
+    private Parent root;
 
-    public void handleButtonClick(){
+    public void handleButtonClick() throws IOException {
+        changeScene("sample.fxml","Grid Pane");
 
+    }
+
+    private void changeScene(String link, String title) throws IOException{
+        stage=(Stage) button.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource(link));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
     }
 }
